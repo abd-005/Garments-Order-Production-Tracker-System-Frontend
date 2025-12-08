997035b9fec325e714e7cf6b64faed60f8823ecc
Md Abdul Ahad <abd.me7@gmail.com>
2025-12-08 22:27:10 +0600

    Setup

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div className='text-2xl bg-red-400'>
+            Hello from Main Layout
+        </div>
+    );
+};
+
+export default MainLayout;
